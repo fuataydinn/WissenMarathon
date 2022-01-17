2bdc920a2bc49ea9df3aa7aadf778a95d329da98
Fuat Aydin <81709694+fuataydinn@users.noreply.github.com>
2022-01-17 21:59:30 +0300

    17.01.2022

---
+++ Programming BasicsPart06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Programming_BasicsPart06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a.Kullanıcıdan istenecek 10 adet sayının içerisinde en büyük ve en küçük olanları tespit edip ekrana sonucu yazan programı geliştirin.
+            //    Kullanıcının yazdığı değerlerin sayı olup olmadığının kontrolü yapılmalıdır. 
+            //    Kullanıcı uygun formatta sayı yazmazsa(“Yirmibeş”, “üç”, “Muhittin”) uyarı döndürülecek ve girilen değer geçersiz sayılacaktır.
+
+            int enKucuk = 0;
+            int enBuyuk = 0;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"{i}. Sayıyı giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi > enBuyuk)
+                {
+                    enBuyuk = sayi;
+                }
+                if (sayi < enKucuk)
+                {
+                    enKucuk = sayi;
+                }
+                if (i == 1)
+                {
+                    enKucuk = sayi;
+                    enBuyuk = sayi;
+                }
+            }
+            Console.WriteLine($"en küçük sayı : {enKucuk}");
+            Console.WriteLine($"en büyük sayı : {enBuyuk}");
+
+
+        }
+    }
+}
