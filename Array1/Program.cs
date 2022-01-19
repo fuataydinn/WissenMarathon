0f9600860005eed983a7f245aea20efce73aaf17
Fuat Aydin <81709694+fuataydinn@users.noreply.github.com>
2022-01-19 15:01:54 +0300

    19.01.2022

---
+++ Array1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi elemanlarını alt alta yazdırma:
+            //Bir dizinin elemanlarını alt alta yazdıran kodu geliştirin.
+
+            //int[] numbers = new int[] { 1, 2, 3 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine($"Dizinin {i+1}.elemanı: {numbers[i]}");
+            //}
+
+            //b.Dizi elemanlarını alt alta tersten yazdırma:
+            //Bir dizinin elemanlarını alt alta ters sırada yazıdan kodu geliştirin.
+
+            //int[] numbers = new int[] { 1, 2, 3 };
+            //for (int i = numbers.Length-1; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"Dizinin {i + 1}.elemanı: {numbers[i]}");
+            //}
+
+            //c.Dizi elemanlarını yan yana yazma
+            //Bir dizinin elemanlarını 45 – 928 – 78 – 4 – 1007 – 83 şeklinde yan yana yazdıran kodu geliştirin. 
+            //Elemanlar arasında mutlaka bir ayraç karakter olmalıdır.İlk elemandan önce veya son elemandan sonra ekranda – (veya diğer bir ayraç) karakteri olmamalıdır.
+
+            //int[] numbers = new int[] { 1, 2, 3 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write($"{numbers[i]}");
+            //    if (i != numbers.Length-1)
+            //    {
+            //        Console.Write("-");
+            //    }
+
+            //}
+
+            //            d.Dizi elemanlarını yan yana ters sırada yazma
+            //            Bir dizinin elemanlarını 83 - 1007 - 4 - 78 - 928 - 45 şeklinde, dizi elemanlarının ters sırasıyla yan yana yazdıran kodu geliştirin.
+            //Elemanlar arasında mutlaka bir ayraç karakter olmalıdır.İlk elemandan önce veya son elemandan sonra ekranda – (veya diğer bir ayraç) karakteri olmamalıdır.
+
+
+            //int[] numbers = new int[] { 1, 2, 3 };
+            //for (int i = numbers.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write($"{numbers[i]}");
+            //    if (i!=0)
+            //    {
+            //        Console.Write("-");
+            //    }
+            //}
+
+        }
+    }
+}
---
+++ Array2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  e.Dizi elemanlarını iki kolonda yazdırma
+            //Dizi elemanlarını sırasıyla iki kolonda yazdıracak kodu geliştirin.Program çıktısında dizi elemanları aşağıdaki sıralamada ekrana getirilecektir.
+            // ***1 * *****2 * **
+            //dizi[0] dizi[1]
+            //dizi[2] dizi[3]
+            //dizi[4] dizi[5]
+            //dizi[6] dizi[7]
+            //NOT1: Konsol çıktısı gösteriminde gerçek elemanlar değil, elemanların dizi içerisinde bulunduğu konum yazılmıştır.
+            //Sıralamanın doğru anlaşılması için gerçek dizi elemanları yazılmamıştır.
+
+            int[] sayi = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("***1***  ***2***");
+            for (int i = 0; i < sayi.Length; i++)
+            {
+               
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
